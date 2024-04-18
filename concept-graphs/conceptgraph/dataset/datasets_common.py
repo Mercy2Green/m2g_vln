2324e5f1eef591c7f488ad492c6838070449a550
Peter Yu <32302140+Mercy2Green@users.noreply.github.com>
2024-04-18 12:18:04 +0800

    1

--- concept-graphs/conceptgraph/dataset/datasets_common.py
+++ concept-graphs/conceptgraph/dataset/datasets_common.py
@@ -23,12 +23,17 @@ import yaml
 from natsort import natsorted
 from scipy.spatial.transform import Rotation as R
 
+from configparser import ConfigParser
+
+
 from gradslam.datasets import datautils
 from gradslam.geometry.geometryutils import relative_transformation
 from gradslam.slam.pointfusion import PointFusion
 from gradslam.structures.rgbdimages import RGBDImages
 
 from conceptgraph.utils.general_utils import to_scalar
+
+
 
 
 def as_intrinsics_matrix(intrinsics):
@@ -89,6 +94,76 @@ def readEXR_onlydepth(filename):
     Y = None if "Y" not in header["channels"] else channelData["Y"]
 
     return Y
+
+
+class R2RDataset(GradSLAMDataset):
+    def __init__(
+        self,
+        config_dict,
+        basedir,
+        sequence,
+        stride: Optional[int] = None,
+        start: Optional[int] = 0,
+        end: Optional[int] = -1,
+        desired_height: Optional[int] = 224,
+        desired_width: Optional[int] = 224,
+        load_embeddings: Optional[bool] = False,
+        embedding_dir: Optional[str] = "embeddings",
+        embedding_dim: Optional[int] = 512,
+        relative_pose: bool = False, # If True, the pose is relative to the first frame
+        scanId, # you need enter scan ID
+        **kwargs,
+    ):
+        self.input_folder = os.path.join(basedir, sequence)
+        self.pose_path = None
+        super().__init__(
+            config_dict,
+            stride=stride,
+            start=start,
+            end=end,
+            desired_height=desired_height,
+            desired_width=desired_width,
+            load_embeddings=load_embeddings,
+            embedding_dir=embedding_dir,
+            embedding_dim=embedding_dim,
+            **kwargs,
+        )
+
+    def get_filepaths(self):
+
+        color_paths = natsorted(glob.glob(f"{self.input_folder}/{self.scanId}/matterport_skybox_images/*.jpg"))
+        depth_paths = natsorted(glob.glob(f"{self.input_folder}/{self.scanId}/undistorted_depth_images/*.png"))
+        embedding_paths = None
+        if self.load_embeddings:
+            embedding_paths = natsorted(
+                glob.glob(f"{self.input_folder}/{self.embedding_dir}/*.pt")
+            )
+
+        # color_paths = natsorted(glob.glob(f"{self.input_folder}/color/*.jpg"))
+        # depth_paths = natsorted(glob.glob(f"{self.input_folder}/depth/*.png"))
+        # embedding_paths = None
+        # if self.load_embeddings:
+        #     embedding_paths = natsorted(
+        #         glob.glob(f"{self.input_folder}/{self.embedding_dir}/*.pt")
+        #     )
+        return color_paths, depth_paths, embedding_paths
+
+    def load_poses(self):
+
+        config = ConfigParser()
+        poses = []
+
+        # poses = []
+        # posefiles = natsorted(glob.glob(f"{self.input_folder}/pose/*.txt"))
+        # for posefile in posefiles:
+        #     _pose = torch.from_numpy(np.loadtxt(posefile))
+        #     poses.append(_pose)
+        return poses
+
+    def read_embedding_from_file(self, embedding_file_path):
+        print(embedding_file_path)
+        embedding = torch.load(embedding_file_path, map_location="cpu")
+        return embedding.permute(0, 2, 3, 1)  # (1, H, W, embedding_dim)
 
 
 class GradSLAMDataset(torch.utils.data.Dataset):
@@ -535,6 +610,7 @@ class ScannetDataset(GradSLAMDataset):
         print(embedding_file_path)
         embedding = torch.load(embedding_file_path, map_location="cpu")
         return embedding.permute(0, 2, 3, 1)  # (1, H, W, embedding_dim)
+
 
 
 class Ai2thorDataset(GradSLAMDataset):
