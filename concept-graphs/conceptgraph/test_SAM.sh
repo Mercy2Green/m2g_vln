d870c49935cfbc75d52e202266eb50e657c523b6
Peter Yu <32302140+Mercy2Green@users.noreply.github.com>
2024-04-24 15:06:22 +0800

    1

--- concept-graphs/conceptgraph/test_SAM.sh
+++ concept-graphs/conceptgraph/test_SAM.sh
@@ -19,23 +19,25 @@
 #     --class_set none 
 
 
-python scripts/generate_gsa_results.py \
-    --dataset_root "/media/m2g/Data/Datasets/dataset/test" \
-    --dataset_config "/media/m2g/Data/Datasets/m2g_vln_server/m2g_vln/concept-graphs/conceptgraph/dataset/dataconfigs/R2R/r2r.yaml" \
-    --scene_id "1LXtFkjw3qL" \
-    --class_set none 
-
-# # The ConceptGraphs-Detect 
 # python scripts/generate_gsa_results.py \
-#     --dataset_root "/data/vln_datasets/matterport3d/v1/unzipped" \
+#     --dataset_root "/data/vln_datasets/matterport3d/test" \
 #     --dataset_config "/home/lg1/peteryu_workspace/m2g_vln/concept-graphs/conceptgraph/dataset/dataconfigs/R2R/r2r.yaml" \
 #     --scene_id "1LXtFkjw3qL" \
-#     --class_set "ram" \
-#     --box_threshold 0.2 \
-#     --text_threshold 0.2 \
-#     --add_bg_classes \
-#     --accumu_classes \
-#     --exp_suffix withbg_allclasses
+#     --class_set none 
+
+# The ConceptGraphs-Detect 
+python scripts/generate_gsa_results.py \
+    --dataset_root "/data/vln_datasets/matterport3d/test" \
+    --dataset_config "/home/lg1/peteryu_workspace/m2g_vln/concept-graphs/conceptgraph/dataset/dataconfigs/R2R/r2r.yaml" \
+    --scene_id "1LXtFkjw3qL" \
+    --class_set "ram" \
+    --box_threshold 0.2 \
+    --text_threshold 0.2 \
+    --add_bg_classes \
+    --accumu_classes \
+    --exp_suffix withbg_allclasses
+
+    # --dataset_root "/data/vln_datasets/matterport3d/v1/unzipped" \
 
 # python scripts/run_slam_rgb.py \
 #     --dataset_root "/media/m2g/Data/Datasets/dataset/test" \
